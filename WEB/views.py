5a6774f1b2e541dd3b5af300a625722d39a150dd
Flickdm <Flickdm@mail.uc.edu>
2016-12-27 07:38:30 +0000

    led strips are still a work in progess

--- WEB/views.py
+++ WEB/views.py
@@ -8,21 +8,54 @@ main = Blueprint('main', __name__)
 @main.route('/index')
 @main.route('/index.html')
 def index():
-    devices = {}
+    output_devices = {}
     for device in hw.output_devices:
-            devices[device] = hw.output_devices.get(device).get_status()
+            output_devices[device] = hw.output_devices.get(device).get_status()
 
-    return render_template('index.html', devices=devices)
+    led_strips = {}
+    for device in hw.led_strips:
+        led_strips["strip"] = {
+                "name": device,
+                "status": hw.led_strips.get(device).get_status(),
+                "colors": hw.led_strips.get(device).get_defined_colors()
+                }
+
+    return render_template(
+        'index.html',
+        output_devices=output_devices,
+        led_strips=led_strips
+        )
 
 @main.route('/add')
 def add_hardware():
     return "add hardware"
 
-@main.route('/devices/', methods=["post"])
-def devices():
-    
-    for device in request.form:
-        if device in hw.output_devices:
-            hw.output_devices.get(device).toggle()
+@main.route('/output_devices/', methods=["post"])
+def output_devices():
+
+    device = request.form.get('device')
+    if device in hw.output_devices:
+        hw.output_devices.get(device).toggle()
+
+    return redirect('/')
+
+@main.route('/led_strips/', methods=["post"])
+def led_strips():
+
+    print(request.form)
+
+    device = request.form.get('device')
+    device_state = request.form.get(device)
+    #print(device_state)
+    color = request.form.get('color')
+
+    if device in hw.led_strips:
+        if device_state == "on":
+            hw.led_strips.get(device).set_color_word(color)
+        elif device_state == "off":
+            hw.led_strips.get(device).set_color_word("none")
+
+
+
 
     return redirect('/')
