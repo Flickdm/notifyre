17acfe251ed2b2fd1612837c64257e3854d2b5a0
Flickdm <Flickdm@mail.uc.edu>
2016-12-27 01:04:47 +0000

    updated web to control leds via switch and iterate over output devices using jinja. Need to add leds

--- WEB/__init__.py
+++ WEB/__init__.py
@@ -3,7 +3,11 @@ from flask import Flask
 app = None
 
 def create_app(app_name):
-    app = Flask(app_name, template_folder='WEB/templates')
+    app = Flask(
+        app_name,
+        template_folder='WEB/templates',
+        static_folder='WEB/static'
+        )
 
     from . import views
     app.register_blueprint(views.main)
--- WEB/views.py
+++ WEB/views.py
@@ -1,13 +1,28 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, redirect
 from HW import get_hardware
 
 hw = get_hardware("gpio")
 main = Blueprint('main', __name__)
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
+@main.route('/index')
+@main.route('/index.html')
 def index():
-    if request.method == 'POST':
-        print(request.form)
-        hw.output_devices["power"].toggle()
+    devices = {}
+    for device in hw.output_devices:
+            devices[device] = hw.output_devices.get(device).get_status()
 
-    return render_template('index.html')
+    return render_template('index.html', devices=devices)
+
+@main.route('/add')
+def add_hardware():
+    return "add hardware"
+
+@main.route('/devices/', methods=["post"])
+def devices():
+    
+    for device in request.form:
+        if device in hw.output_devices:
+            hw.output_devices.get(device).toggle()
+
+    return redirect('/')
